cc509dd337193bbd7314ed9ad00ba0521cf5bb10
alftunner <41523898+alftunner@users.noreply.github.com>
2020-04-29 20:17:21 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -15,6 +15,10 @@ namespace Date
             Month = month;
             Year = year;
         }
+        public void showDate()
+        {
+            WriteLine($"Date: {Day}.{Month}.{Year}");
+        }
     }
     public class DateCalc
     {
@@ -62,6 +66,71 @@ namespace Date
                 return dayDiff;
             }
         }
+
+        public static Date addDay(Date date1, int days) // Добавляет к дате заданное количество дней и возвращает новую дату.
+        {
+            days += DateCalc.DayCount(date1);
+            date1.Year = days / 365;
+            days = days % 365;
+            date1.Month = 0;
+            for (int i = 1; i < 12; i++)
+            {
+                if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12)
+                {
+                    if(days/31 > 0)
+                    {
+                        date1.Month += 1;
+                        days -= 31;
+                    }
+                    else
+                    {
+                        date1.Day = days;
+                        break;
+                    }
+                    
+                }
+                if (i == 4 || i == 6 || i == 9 || i == 11)
+                {
+                    if (days / 30 > 0)
+                    {
+                        date1.Month += 1;
+                        days -= 30;
+                    }
+                    else
+                    {
+                        date1.Day = days;
+                        break;
+                    }
+                }
+                if (i == 2 && date1.Year % 4 == 0)
+                {
+                    if (days / 29 > 0)
+                    {
+                        date1.Month += 1;
+                        days -= 29;
+                    }
+                    else
+                    {
+                        date1.Day = days;
+                        break;
+                    }
+                }
+                if (i == 2 && date1.Year % 4 != 0)
+                {
+                    if (days / 28 > 0)
+                    {
+                        date1.Month += 1;
+                        days -= 28;
+                    }
+                    else
+                    {
+                        date1.Day = days;
+                        break;
+                    }
+                }
+            }
+            return date1;
+        }
     }
     class Program
     {
@@ -69,7 +138,10 @@ namespace Date
         {
             Date date1 = new Date(25, 4, 2020);
             Date date2 = new Date(28, 4, 2020);
+            Date date3 = new Date();
             WriteLine($"Разность дней: {DateCalc.DayDiff(date1, date2)}");
+            date3 = DateCalc.addDay(date2, 487);
+            date3.showDate();
         }
     }
 }
